0c02fc27a73323d9bbf9c1723c619262ee3e4819
nguyenvukhang <brew4k@gmail.com>
2024-01-30 23:43:59 +0800

    drop: WrappedSshChild

--- mux/src/ssh.rs
+++ mux/src/ssh.rs
@@ -1,5 +1,4 @@
 use crate::Mux;
-use config::{SshBackend, SshDomain};
 use portable_pty::{ChildKiller, ExitStatus};
 use smol::channel::{bounded, Receiver as AsyncReceiver};
 use std::io::{Read, Write};
@@ -7,7 +6,7 @@ use std::sync::mpsc::{Receiver, TryRecvError};
 use std::sync::{Arc, Mutex};
 use termwiz::cell::unicode_column_width;
 use termwiz::lineedit::*;
-use wezterm_ssh::{ConfigMap, SshChildProcess};
+use wezterm_ssh::SshChildProcess;
 
 #[derive(Default)]
 struct PasswordPromptHost {
@@ -39,51 +38,6 @@ impl LineEditorHost for PasswordPromptHost {
     }
 }
 
-pub fn ssh_domain_to_ssh_config(ssh_dom: &SshDomain) -> anyhow::Result<ConfigMap> {
-    let mut ssh_config = wezterm_ssh::Config::new();
-    ssh_config.add_default_config_files();
-
-    let (remote_host_name, port) = {
-        let parts: Vec<&str> = ssh_dom.remote_address.split(':').collect();
-
-        if parts.len() == 2 {
-            (parts[0], Some(parts[1].parse::<u16>()?))
-        } else {
-            (ssh_dom.remote_address.as_str(), None)
-        }
-    };
-
-    let mut ssh_config = ssh_config.for_host(&remote_host_name);
-    ssh_config.insert(
-        "wezterm_ssh_backend".to_string(),
-        match ssh_dom
-            .ssh_backend
-            .unwrap_or_else(|| config::configuration().ssh_backend)
-        {
-            SshBackend::Ssh2 => "ssh2",
-            SshBackend::LibSsh => "libssh",
-        }
-        .to_string(),
-    );
-    for (k, v) in &ssh_dom.ssh_option {
-        ssh_config.insert(k.to_string(), v.to_string());
-    }
-
-    if let Some(username) = &ssh_dom.username {
-        ssh_config.insert("user".to_string(), username.to_string());
-    }
-    if let Some(port) = port {
-        ssh_config.insert("port".to_string(), port.to_string());
-    }
-    if ssh_dom.no_agent_auth {
-        ssh_config.insert("identitiesonly".to_string(), "yes".to_string());
-    }
-    if let Some("true") = ssh_config.get("wezterm_ssh_verbose").map(|s| s.as_str()) {
-        log::info!("Using ssh config: {ssh_config:#?}");
-    }
-    Ok(ssh_config)
-}
-
 #[derive(Debug)]
 struct KillerInner {
     killer: Option<Box<dyn ChildKiller + Send + Sync>>,
@@ -96,139 +50,6 @@ struct KillerInner {
 #[derive(Debug, Clone)]
 struct WrappedSshChildKiller {
     inner: Arc<Mutex<KillerInner>>,
-}
-
-#[derive(Debug)]
-pub(crate) struct WrappedSshChild {
-    status: Option<AsyncReceiver<ExitStatus>>,
-    rx: Receiver<SshChildProcess>,
-    exited: Option<ExitStatus>,
-    killer: WrappedSshChildKiller,
-}
-
-impl WrappedSshChild {
-    fn check_connected(&mut self) {
-        if self.status.is_none() {
-            match self.rx.try_recv() {
-                Ok(c) => {
-                    self.got_child(c);
-                }
-                Err(TryRecvError::Empty) => {}
-                Err(err) => {
-                    log::debug!("WrappedSshChild::check_connected err: {:#?}", err);
-                    self.exited.replace(ExitStatus::with_exit_code(1));
-                }
-            }
-        }
-    }
-
-    fn got_child(&mut self, mut child: SshChildProcess) {
-        {
-            let mut killer = self.killer.inner.lock().unwrap();
-            killer.killer.replace(child.clone_killer());
-            if killer.pending_kill {
-                let _ = child.kill().ok();
-            }
-        }
-
-        let (tx, rx) = bounded(1);
-        promise::spawn::spawn_into_main_thread(async move {
-            if let Ok(status) = child.async_wait().await {
-                tx.send(status).await.ok();
-                let mux = Mux::get();
-                mux.prune_dead_windows();
-            }
-        })
-        .detach();
-        self.status.replace(rx);
-    }
-}
-
-impl portable_pty::Child for WrappedSshChild {
-    fn try_wait(&mut self) -> std::io::Result<Option<ExitStatus>> {
-        if let Some(status) = self.exited.as_ref() {
-            return Ok(Some(status.clone()));
-        }
-
-        self.check_connected();
-
-        if let Some(rx) = self.status.as_mut() {
-            match rx.try_recv() {
-                Ok(status) => {
-                    self.exited.replace(status.clone());
-                    Ok(Some(status))
-                }
-                Err(smol::channel::TryRecvError::Empty) => Ok(None),
-                Err(err) => {
-                    log::debug!("WrappedSshChild::try_wait err: {:#?}", err);
-                    let status = ExitStatus::with_exit_code(1);
-                    self.exited.replace(status.clone());
-                    Ok(Some(status))
-                }
-            }
-        } else {
-            Ok(None)
-        }
-    }
-
-    fn wait(&mut self) -> std::io::Result<portable_pty::ExitStatus> {
-        if let Some(status) = self.exited.as_ref() {
-            return Ok(status.clone());
-        }
-
-        if self.status.is_none() {
-            match smol::block_on(async { self.rx.recv() }) {
-                Ok(c) => {
-                    self.got_child(c);
-                }
-                Err(err) => {
-                    log::debug!("WrappedSshChild err: {:#?}", err);
-                    let status = ExitStatus::with_exit_code(1);
-                    self.exited.replace(status.clone());
-                    return Ok(status);
-                }
-            }
-        }
-
-        let rx = self.status.as_mut().unwrap();
-        match smol::block_on(rx.recv()) {
-            Ok(status) => {
-                self.exited.replace(status.clone());
-                Ok(status)
-            }
-            Err(err) => {
-                log::error!("WrappedSshChild err: {:#?}", err);
-                let status = ExitStatus::with_exit_code(1);
-                self.exited.replace(status.clone());
-                Ok(status)
-            }
-        }
-    }
-
-    fn process_id(&self) -> Option<u32> {
-        None
-    }
-
-    #[cfg(windows)]
-    fn as_raw_handle(&self) -> Option<std::os::windows::io::RawHandle> {
-        None
-    }
-}
-
-impl ChildKiller for WrappedSshChild {
-    fn kill(&mut self) -> std::io::Result<()> {
-        let mut killer = self.killer.inner.lock().unwrap();
-        if let Some(killer) = killer.killer.as_mut() {
-            killer.kill()
-        } else {
-            killer.pending_kill = true;
-            Ok(())
-        }
-    }
-
-    fn clone_killer(&self) -> Box<dyn ChildKiller + Send + Sync> {
-        Box::new(self.killer.clone())
-    }
 }
 
 impl ChildKiller for WrappedSshChildKiller {
